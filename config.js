f406233703fa783632811c6cff51535c3eb9debe
ThenBao <88052323+ThenBao@users.noreply.github.com>
2021-07-28 11:03:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Bồ ơi bồ à!',
+    introTitle: 'Bồ ơi!',
     introDesc: `Bé bt bé hay suy nghĩ này nọ nhưng
     bé yêu anh nhìu lắm mong anh hỉu bé nhìu hơn 
     cố gắng cùng nhau hết dịch để gặp nhau ha
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho Bé <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Bé biết mà 🥰. Yêu Bồ nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu Bồ nhiều nhiều 😘😘',
     messDesc: 'Bồ có iu bé nhìu hem .',
     btnAccept: 'Iu nhìu lắm<3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
